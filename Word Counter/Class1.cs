c6fdd3fa676801d74c1ef658ec5d2e73e88d5a5b
KadinMeyer23 <vq4717wg@go.minnstate.edu>
2025-02-18 17:07:39 -0600

    Add files via upload

---
+++ Word Counter/Class1.cs
@@ -0,0 +1,29 @@
+﻿//Kadin Meyer 2/18/2025
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Word_Counter
+{
+    public class Count
+    {
+
+        public int word_count(string Sentence) {
+            int total = 1;
+
+            foreach (char c in Sentence)
+            {
+                if (c != ' ') continue;
+                else { total++; }
+            }
+
+
+            return total;
+        }
+
+    }
+}
---
+++ Word Counter/Program.cs
@@ -0,0 +1,14 @@
+﻿//Kadin Meyer 2/18/2025
+
+namespace Word_Counter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Count acount = new Count();
+            string sentence = Console.ReadLine();
+            Console.WriteLine(acount.word_count(sentence));
+        }
+    }
+}
